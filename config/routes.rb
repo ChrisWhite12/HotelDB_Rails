3b38b1d1c9380c848f36a82d8ba04a47aa1de3a9
ChrisWhite12 <chris_white_12@hotmail.com>
2020-07-28 15:13:58 +1000

    added booking and profile pages, added sign in and out in nav

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   #Show all listings
-  get "/listing", to: "listings#index", as: "listings"
+  get "/listings", to: "listings#index", as: "listings"
   #create listing
   get "/listings/new", to: "listings#new", as: "new_listing"
   #show listing
   get "/listings/:id", to: "listings#show", as: "listing"
+  #edit listing
+  get "/listings/:id/edit", to: "listings#edit", as: "edit_listing"
   #create listing
   post "/listings", to: "listings#create", as: "create_listing"
   #delete listing
@@ -17,5 +19,19 @@ Rails.application.routes.draw do
   get "/", to: "pages#home", as:"root"
   #results
   get "/search", to: "pages#search", as:"search"
+  #result
+  get "/search/:id", to: "pages#result", as:"result"
+
+
+  #bookings
+  get "/bookings", to: "bookings#index", as: "bookings"  
+
+  #profile
+  get "/profiles", to: "profiles#index", as: "profiles"
+  #view profile
+  get "/profiles/:id", to: "profiles#show", as: "profile"
+  #edit profile
+  get "/profiles/:id/edit", to: "profiles#edit", as: "edit_profile"
+
 
 end
